6b6e51126e8cc4dcce4b80b25767f25dd4b54b93
Sakshi <sakshipatare03@gmail.com>
2025-08-30 19:12:00 +0530

    Added backend project files

---
+++ server.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import { connectUsingMongoose } from "./src/config/mongooseConfig.js";
+import userRouter from "./src/features/user/user.routes.js";
+
+
+import cors from 'cors';
+
+const server = express();
+
+// ✅ Add CORS with specific options
+server.use(cors({
+  origin: '*', // Frontend's Vite dev server
+  credentials: true               // Allow cookies/auth headers if needed
+}));
+
+server.use(express.json());
+
+server.use('/users', userRouter);
+
+
+server.listen(4000, () => {
+  console.log("✅ Server is running at 4000");
+  connectUsingMongoose();
+});
